4ec83812d70eccb8926608500ff8ff360e532a62
Bhagesh Kalekar <bhagesh.kalekar@gmail.com>
2017-12-13 17:52:58 +0530

    Add files via upload

--- src/app/pages/movies/add-movie.js
+++ src/app/pages/movies/add-movie.js
@@ -1,65 +1,94 @@
 import React, { Component } from 'react';
 import PageTitle from '../../components/page-title/page-title';
-import { Field, reduxForm } from 'redux-form';
+import { Field, FieldArray, reduxForm } from 'redux-form';
 import * as validationRules from './validations';
-import renderField from '../../components/feilds/movie';
-
-
+import simpleNumberLocalizer from 'react-widgets-simple-number';
+import renderField from '../../components/feilds/feild';
+import renderDateTimePicker from '../../components/feilds/datetimepicker';
+import RenderNumberPickerGroup from '../../components/feilds/numberpickergroup';
+import renderCasts from '../../components/movie/casts';
+import renderMultiSelect from '../../components/feilds/multiselect';
+import Moment from 'moment';
+import momentLocalizer from 'react-widgets-moment';
+import 'react-widgets/dist/css/react-widgets.css';
+Moment.locale();
+momentLocalizer();
+simpleNumberLocalizer();
 class AddMovie extends Component {
     addMovie = (values) => {
        alert(JSON.stringify(values));
     }
     render() {
-        const { handleSubmit, pristine, reset, submitting } = this.props;
+        const { handleSubmit, pristine, reset, submitting, title } = this.props;
         return(
             <div>
-                <PageTitle title={this.props.title}/>
+                <PageTitle title={title}/>
                 <div className="form-container">
                     <form onSubmit={handleSubmit(this.addMovie)}>
                         <Field
                             name="title"
                             type="text"
                             component={renderField}
-                            label="Movie Title"
+                            label="Movie title"
                             validate={[validationRules.required]}
                         />
                         <Field
                             name="subtitle"
                             type="text"
                             component={renderField}
-                            label="Sub Title"
+                            label="Sub title"
                         />
                         <Field
                             name="genres"
                             type="text"
                             component={renderField}
                             label="Genres"
-                            placeholder="Add comma separated Genres"
+                            placeholder="Add comma separated genres"
                             validate={[validationRules.required]}
                         />
-                        <Field
-                            name="runningtime"
-                            type="text"
-                            component={renderField}
+                        <RenderNumberPickerGroup
                             label="Running time"
-                            validate={[validationRules.required]}
+                            labelone="Hours"
+                            labeltwo="Minutes"
+                            defaultvalueone="0"
+                            defaultvaluetwo="0"
+                            minone="0"
+                            maxone="5"
+                            mintwo="0"
+                            maxtwo="59"
                         />
                         <Field
                             name="releasedate"
-                            type="text"
-                            component={renderField}
-                            label="Release date"
-                            validate={[validationRules.required]}
+                            component={renderDateTimePicker}
+                            label="Movie release date"
+                            showTime="false"
                         />
+                        <Field
+                            label="Directors"
+                            name="directors"
+                            placeholder="Add directors"
+                            component={renderMultiSelect}
+                            textFeild={item => item}
+                            data={[ 'Guitar', 'Cycling', 'Hiking' ]}
+                        />
+                        <Field
+                            label="Writers"
+                            name="writers"
+                            placeholder="Add writers"
+                            component={renderMultiSelect}
+                            textFeild={item => item}
+                            data={[ 'Guitar', 'Cycling', 'Hiking' ]}
+                        />
+                        <FieldArray name="cast" component={renderCasts}/>
                         <Field
                             name="tarilerlink"
                             type="text"
                             component={renderField}
-                            label="Trailer link"
+                            label="Movie trailer link"
                             validate={[validationRules.required,validationRules.url]}
                         />
                         <div className="text-right">
-                            <button type="submit" className="btn btn-sm btn-primary">Add Movie</button>
+                            <button type="submit" className="btn btn-sm btn-primary" disabled={submitting || pristine}>Add Movie</button>
                         </div>
                     </form>
                 </div>
